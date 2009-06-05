1ccca4200e5eee387438342004d8674f22afb2b8
msofaer <msofaer@inigral.com>
2009-06-04 19:13:04 -0700

    Starting to work in dev.  Json gem fail in prod

--- app.rb
+++ app.rb
@@ -1,6 +1,27 @@
 require 'rubygems'
 require 'sinatra'
+require 'lib/waveapi/init'
  
 get '/' do
   "Hello from Sinatra running on Java!"
+end
+
+get '/_wave/capabilities.xml' do
+  begin
+    robot.capabilities.to_s
+  rescue
+    $!
+  end
+end
+
+get '/_wave/robot/:command' do
+  robot.run_command(params[:command])
+end
+
+get '/ping' do
+  robot.to_s
+end
+
+def robot
+  Robot.new('Bob')
 end
