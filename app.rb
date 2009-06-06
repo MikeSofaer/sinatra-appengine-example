bbc28773b9b815d2589ac37447deee56cfc505db
msofaer <msofaer@inigral.com>
2009-06-05 19:06:47 -0700

    Trying to make the JSON parse

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require 'rubygems'
 require 'sinatra'
 require 'lib/waveapi/init'
 require 'appengine-apis/logger'
+require 'lib/json'
+require 'json'
+import org.json.JSONObject
  
 get '/' do
   "I AM WAVE ROBOT! I sing like Frank on JRuby!"
@@ -13,31 +16,8 @@ get '/_wave/capabilities.xml' do
 end
 
 post '/_wave/robot/jsonrpc' do
-  logger.info "request " << request.to_s
-  logger.info "request body " << request.body.to_s
-  logger.info "request body class " << request.body.class.name
-  logger.info "request env " << request.env.to_s
-  logger.info "request env keys " << request.env.keys.join(' .. ').to_s
-  logger.info "request rack input " << request.env["rack.input"].to_s
-  logger.info "request rack request " << request.env["rack.request"].to_s
-  logger.info "request rack request post " << request.env["rack.request"].POST.to_s
-  logger.info "request rack request post class" << request.env["rack.request"].POST.class.name
-  logger.info "request rack request form input" << request.env["rack.request.form_input"].read.to_s
-  logger.info "request rack request form hash" << request.env["rack.request.form_hash"].to_s
-  logger.info "request rack request form hash keys" << request.env["rack.request.form_hash"].keys.join(' .. ').to_s
-  logger.info "request rack request query string" << request.env["rack.request.query_string"].to_s
-  logger.info "request rack servlet request" << request.env["java.servlet_request"].to_s
-  logger.info "request rack servlet input stream" << request.env["java.servlet_request"].getInput.to_s
-    logger.info "request rack servlet input stream value " << request.env["java.servlet_request"].getInput.read.to_s
-	logger.info "Recreate json? " << request.env["rack.request"].POST.to_json
-  
-  raw = request.env["rack.input"].read
-  logger.info "raw " << raw.to_s
-  logger.info "Request params " << params.to_s
-  logger.info "Params class " << params.class.name
-  logger.info "Request param keys" << params.keys.map{|k| k.to_s}.join("  ...  ")
-  logger.info "Command not yet found"
-  logger.info "craziness " << JSON(request.env["rack.request"].POST)
+  json = JSONObject.new(params.to_json)
+  logger.info "parsed? " << Robot.ParseJSONBody(json).to_s
   robot.run_rpc(params)
 end
 
